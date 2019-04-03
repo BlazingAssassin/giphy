5e4780fda7ffbd9baf3eb1225b926e4f532acf93
Damon Nunez <dameplays24@gmail.com>
2019-04-03 21:51:01 +0000

    hehe

--- js/script.js
+++ js/script.js
@@ -4,14 +4,16 @@
 /* global $ */
 
 $("#search-button").click(function(){
+     var search= $("input").val();
+    var request_url ="https://api.giphy.com/v1/gifs/search?q=" + search + "&rating=pg&api_key=dc6zaTOxFJmzC";
    $.ajax({
-        url: "https://api.giphy.com/v1/gifs/search?q=puppy&rating=pg&api_key=dc6zaTOxFJmzC",
+        url: request_url,
         method: "GET",
         success: function(response) {
             console.log(response);
             $("body").append("<img src=" + "https://media3.giphy.com/media/4nGYmLsCoHNXa/200w.gif?cid=" +search +  "e1bb72ff5ca5184f78614f724d030c1e"
             + ">");
-            var search= $("input").val();
+           
             
             console.log(search);
             
